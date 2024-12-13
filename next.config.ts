25e1257be43fae0762f357e46a54a115ac6432c7
toshiyuki-nakazato-embit <toshiyuki.nakazato@embit.co.jp>
2024-12-13 17:58:45 +0900

    fix

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   experimental: {
     serverActions: {
-      bodySizeLimit: "2mb",
-    },
-  },
-  output: "export", // 静的エクスポートを有効化
-  trailingSlash: true, // 必要に応じてパス末尾にスラッシュを付与
-};
+      bodySizeLimit: '2mb'
+    }
+  }
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
